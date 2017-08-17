4c3d2a06d630a1e00dd6fafbf49938f5cddbf9a9
十百千 <x1001000@users.noreply.github.com>
2017-08-17 16:15:54 +0800

    新手訓練 ~ 陣列應用

--- a248.py
+++ a248.py
@@ -1,12 +1,11 @@
 while True:
   try:
-    a,b,N=input().split()
-    a,b,N=int(a),int(b),int(N)
-    answer=str(10**N*a//b)
-    n=len(answer)
-    if N>=n:
-      print('0.'+'0'*(N-n)+answer[-N:])
-    else:
-      print(answer[:-N]+'.'+answer[-N:])
+    a,b,N=map(int,input().split())
   except:
     break
+  answer=str(10**N*a//b)
+  n=len(answer)
+  if N>=n:
+    print('0.'+'0'*(N-n)+answer[-N:])
+  else:
+    print(answer[:-N]+'.'+answer[-N:])
