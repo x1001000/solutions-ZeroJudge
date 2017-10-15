cadc78643313563ca291c5906f7caa69850a9a2c
十百千 <x1001000@users.noreply.github.com>
2017-10-15 02:48:53 -0500

    APCS 2017-0304-2小群體
    if x not in Y 可能是搜尋導致TLE

--- c291.py
+++ c291.py
@@ -1,17 +1,29 @@
 while True:
   try:
-    input()
+    N=int(input())
   except:
     break
-  X=input().split()
-  Y=[]
+  X=[int(x) for x in input().split()]
+  Y=[True for _ in range(N)]
   group=0
   for x in X:
-    if x not in Y:
-      Y.append(x)
-      index=int(x)
-      while X[index]!=x:
-        Y.append(X[index])
-        index=int(X[index])
+    if Y[x]:
+      Y[x]=False
+      friend=X[x]
+      while not friend==x:
+        Y[friend]=False
+        friend=X[friend]
       group+=1
   print(group)
+'''
+輸入字串轉成整數存入N
+發生EOF時跳出迴圈
+X是好友編號的list
+Y是待追蹤狀態的list
+group是小群體個數
+對於X的每一項x
+若待追蹤則進入追蹤流程
+待追蹤狀態改成False
+追蹤回x就跳出while迴圈然後小群體個數加一
+輸出小群體個數
+'''
