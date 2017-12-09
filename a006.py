2f5ca092c74616c8585717df624f41c6250b015e
x1001000 <x1001000@gmail.com>
2017-12-10 02:01:41 +0800

    a006

---
+++ a006.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        a,b,c = input().split()
+    except:
+        break
+    a,b,c = int(a),int(b),int(c)
+    D = b**2 - 4*a*c
+    if D > 0:
+        x1 = (-b + D**0.5) / (2*a)
+        x2 = (-b - D**0.5) / (2*a)
+        x1,x2 = int(x1), int(x2)
+        print('Two different roots x1={} , x2={}'.format(x1,x2))
+    elif D==0:
+        x = -b / (2*a)
+        x = int(x)
+        print('Two same roots x={}'.format(x))
+    else:
+        print('No real root')
