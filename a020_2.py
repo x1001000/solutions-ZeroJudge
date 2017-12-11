0ee7485c9335dade60ec25511a6a5997b6229a34
x1001000 <x1001000@gmail.com>
2017-12-11 14:37:40 +0800

    a020

--- a020_2.py
+++ a020_2.py
@@ -4,13 +4,13 @@ while True:
         ID = input()
     except:
         break
-    index = ord(ID[0])-ord('A')
+    index = ord(ID[0]) - ord('A')
     add_up = (number[index]%10)*9 + number[index]//10
     n = 8
     for x in ID[1:-1]:
         add_up += int(x)*n
         n -= 1
-    add_up = add_up + int(ID[-1])
+    add_up += int(ID[-1])
     if add_up%10 == 0:
         print('real')
     else:
