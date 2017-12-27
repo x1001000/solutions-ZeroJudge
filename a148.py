770b19b502a5dd81a0407b7dc51f6c35733d4983
x1001000 <x1001000@gmail.com>
2017-12-27 22:36:03 +0800

    a148

--- a148.py
+++ a148.py
@@ -3,8 +3,9 @@ while True:
         line = input()
     except:
         break
-    integers = [ int(string) for string in line.split() ]
-    average = sum(integers[1:])/integers[0]
+    strings = line.split()
+    integers = [ int(string) for string in strings ]
+    average = sum(integers[1:]) / integers[0]
     if average > 59:
         print('no')
     else:
