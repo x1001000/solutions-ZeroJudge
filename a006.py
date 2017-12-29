73be4725051f2c40e1305869a03529d107e489fa
x1001000 <x1001000@gmail.com>
2017-12-29 13:23:05 +0800

    line.split()

--- a006.py
+++ a006.py
@@ -9,7 +9,7 @@ while True:
     if D > 0:
         x1 = (-b + D**0.5) / (2*a)
         x2 = (-b - D**0.5) / (2*a)
-        x1,x2 = int(x1), int(x2)
+        x1, x2 = int(x1), int(x2)
         print('Two different roots x1={} , x2={}'.format(x1,x2))
     elif D==0:
         x = -b / (2*a)
