119f4f888ad384654ae6476779854b4e327e10b9
十百千 <x1001000@users.noreply.github.com>
2019-08-14 00:14:13 +0800

    Update a040.py

--- a040.py
+++ a040.py
@@ -1,26 +1,19 @@
 def is_Armstrong_number(n):
-    S = str(n)
-    L = len(S)
     x = 0
-    for i in S:
-        x += int(i)**L
-    if x == n:
-        return True
-    else:
-        return False
+    for s in str(n):
+        x += int(s)**len(str(n))
+    return True if x == n else False
 
 while True:
     try:
-        line = input()
+        n, m = map(int, input().split())
     except:
         break
-    n, m = line.split()
-    n, m = int(n), int(m)
-    no_Armstrong_number = True
+    there_is_none = True
     for i in range(n,m+1):
         if is_Armstrong_number(i):
+            there_is_none = False
             print(i,end=' ')
-            no_Armstrong_number = False
-    if no_Armstrong_number:
+    if there_is_none:
         print('none',end='')
     print()
