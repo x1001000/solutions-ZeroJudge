df08e748a039313ce1ff5b346a2561cab98e9718
十百千 <x1001000@users.noreply.github.com>
2019-11-10 13:22:01 +0800

    Update a001.py

--- a001.py
+++ a001.py
@@ -1,6 +1,5 @@
-while True:               #因為不知測資有幾行，所以用無窮迴圈
-    try:                  #try/except是例外處理，當try失敗
-        line = input()    #若成功輸入一行字串，就存入變數line
-    except:               #就會進入except
-        break             #break會跳出while迴圈
-    print('hello,', line) #然後印出字串'hello'和變數line
+while True:                     #因為不知測資有幾行，所以用無窮迴圈
+    try:                        #try/except是例外處理的語法
+        print('hello,', input())#印出hello和輸入字串
+    except:                     #直到EOFError發生
+        break                   #break無窮迴圈
