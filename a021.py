f8e6a1d2b56304467488fde5c51a854621d4b581
十百千 <x1001000@users.noreply.github.com>
2020-12-30 07:57:53 +0800

    Update a021.py

--- a021.py
+++ a021.py
@@ -1,13 +1,11 @@
-while True:
-    try:
-        s1, op, s2 = input().split()
-    except:
-        break
-    if op == '+':
-        print(int(s1) + int(s2))
-    if op == '-':
-        print(int(s1) - int(s2))
-    if op == '*':
-        print(int(s1) * int(s2))
-    if op == '/':
-        print(int(s1) // int(s2))
+#print(eval(input().replace('/', '//')))
+
+s1, op, s2 = input().split()
+if op == '+':
+    print(int(s1) + int(s2))
+if op == '-':
+    print(int(s1) - int(s2))
+if op == '*':
+    print(int(s1) * int(s2))
+if op == '/':
+    print(int(s1) // int(s2))
