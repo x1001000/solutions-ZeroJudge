91c382325a8218ba24adf93e34ea251b97c3a06c
十百千 <x1001000@users.noreply.github.com>
2021-05-06 20:33:58 +0800

    Update a004.py

--- a004.py
+++ a004.py
@@ -1,8 +1,10 @@
 while True:
     try:
-        year = int(input()) #輸入轉整數存入year
+        year = int(input()) # 輸入轉整數存入year
     except:
-        break  #⬇︎西元年被4整除且不被100整除，或被400整除者即為閏年
+        break               # 中斷無窮迴圈才能在限定時間內跑完腳本
+
+    # 西元年被4整除且不被100整除，或被400整除者即為閏年
     if (year%4==0 and year%100!=0) or year%400==0:
         print('閏年')
     else:
